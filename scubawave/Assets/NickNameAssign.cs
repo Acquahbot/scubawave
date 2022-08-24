bdf4cea7dcbf4f1b96ae96bb1006c390adc73e75
Acquahbot <acquahbot@gmail.com>
2022-08-24 12:29:41 +0200

    lobbyPlayerList

---
+++ scubawave/Assets/NickNameAssign.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using TMPro;
+
+public class NickNameAssign : MonoBehaviour
+{
+    PhotonView PV;
+    TMP_Text textInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PV = GetComponent<PhotonView>();
+        textInput = GetComponent<TMP_Text>();
+
+        if (PV.IsMine)
+        {
+            textInput.text = PhotonNetwork.NickName;
+        }
+        else
+        {
+            textInput.text = PV.Owner.NickName;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- scubawave/Assets/Scripts/Acquah/SpawnPlayersNicks.cs
+++ scubawave/Assets/Scripts/Acquah/SpawnPlayersNicks.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class SpawnPlayersNicks : MonoBehaviour
 {
+    public GameObject playerNickPrefab;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector3 position = new Vector3(0f, 0f, 0f);
+        PhotonNetwork.Instantiate(playerNickPrefab.name,position, Quaternion.identity);
     }
 
     // Update is called once per frame
